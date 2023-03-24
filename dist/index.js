70c4010f71fe999a4910da3aef3685d3838e28bc
arsalon-amini <arsalonamini2020@gmail.com>
2023-03-24 23:16:21 +0800

    enums

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,5 @@
 "use strict";
-let sales = 123434;
-let monthly_sales = 123456;
-let course = 'Typescript';
-let monthly_course = 'dont require to annotate because typescript does it';
+;
+let mySize = 3;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
