ceac32a8d97dc7026fd423779e7735bce66f2005
arsalon-amini <arsalonamini2020@gmail.com>
2023-03-24 23:27:11 +0800

    Typescript: functions

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
-let numbers: number[] = [1, 2, 3, '4'];
-let sales = [120, 250, 340];
 
+function calculateTax(income: number, taxYear?: number): number {
+    if(taxYear < 2022)
+        return income * 1.2;
+    if(income < 50_000)
+        return income * 1.2;
 
+    return 0;
+}
+
+calculateTax(100_000, 2022) 
